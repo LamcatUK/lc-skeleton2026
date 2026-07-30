55f335d938d981b668a951d9fd94a69c5dba93af
LamcatUK <136230172+LamcatUK@users.noreply.github.com>
2026-07-30 10:55:11 +0100

    footer

--- footer.php
+++ footer.php
@@ -1,17 +1,23 @@
 </main>
 
-<footer id="colophon" class="container">
-	<?php
-	wp_nav_menu(
-		array(
-			'theme_location' => 'footer',
-			'menu_class'     => 'navbar-nav',
-			'container'      => false,
-			'fallback_cb'    => false,
-		)
-	);
-	?>
-	<p>&copy; <?php echo esc_html( gmdate( 'Y' ) ); ?> <?php bloginfo( 'name' ); ?></p>
+<footer id="footer">
+	<div class="container">
+		<?php
+		wp_nav_menu(
+			array(
+				'theme_location' => 'footer',
+				'menu_class'     => 'navbar-nav',
+				'container'      => false,
+				'fallback_cb'    => false,
+			)
+		);
+		?>
+	</div>
+	<div id="colophon">
+		<div class="container">
+			<p>&copy; <?php echo esc_html( gmdate( 'Y' ) ); ?> <?php bloginfo( 'name' ); ?></p>
+		</div>
+	</div>
 </footer>
 
 <?php wp_footer(); ?>
